06db8fc895528481a98f03fdb15afaea852a9407
SyntaxError <97899734+szumaster@users.noreply.github.com>
2022-06-03 16:21:22 +0200

    #41 [03/06/2022]
    - add basic instance files

---
+++ MODPACK/kubejs/server_scripts/script.js
@@ -0,0 +1,14 @@
+// priority: 50
+
+settings.logAddedRecipes = false
+settings.logRemovedRecipes = false
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+onEvent('recipes', event => {
+
+})
+
+onEvent('item.tags', event => {
+	
+})
